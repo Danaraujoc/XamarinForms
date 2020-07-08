21f23cead55cecae4d914a06d65ab2ee6b398abb
daniel.araujox77 <daniel.araujox77@hotmail.com>
2020-07-07 22:22:35 -0300

    App01  Concluído - Udemy

---
+++ Seção 05/App1_ConsultarCep/App1_ConsultarCep/Serviço/ViaCEPServico.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using App1_ConsultarCep.Serviço.Modelo;
+using Newtonsoft.Json;
+
+namespace App1_ConsultarCep.Serviço
+{
+    public class ViaCEPServico
+    {
+        private static string EnderecoURL = "http://viacep.com.br/ws/{0}/json/";
+
+        public static Endereco BuscarEnderecoViaCep(string cep)
+        {
+            string NovoEnderecoURL = string.Format(EnderecoURL, cep);
+
+            WebClient wc = new WebClient();
+
+            string Conteudo = wc.DownloadString(NovoEnderecoURL);
+
+            Endereco end = JsonConvert.DeserializeObject<Endereco>(Conteudo);
+
+            if (end.cep == null) return null;
+
+            return end;
+
+
+        }
+    }
+}
